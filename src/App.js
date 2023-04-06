e943b08399bc7010e71adeb986b29ac2626f31d7
Govinda Biswas <govindbiswas079@gmail.com>
2023-04-06 20:26:41 +0530

    NOKIA PureBook-S14

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div style={{
+      height: '100vh',
+      backgroundColor: 'blue',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <h3 style={{
+        color: '#FFFFFF',
+        fontFamily: 'sans-serif'
+      }}>NOKIA PureBook-S14</h3>
+    </div>
+  )
+}
+
+export default App
